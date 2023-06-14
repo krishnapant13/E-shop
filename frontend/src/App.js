5c817348606f44feb8eb51b82ee5ad5886f0cb7a
krishnapant13 <krishna555.pant@gmail.com>
2023-06-14 11:47:22 +0530

    Frontend Login Signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import "./App.css"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {LoginPage,SignupPage} from './Routes'
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/sign-up' element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
